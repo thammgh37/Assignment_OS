485ed41682206c75425e27baae9645e5f2afeeba
PhongVietCode <quocphogn1101@gmail.com>
2023-12-05 00:48:24 +0700

    merge

--- include/os-cfg.h
+++ include/os-cfg.h
@@ -1,13 +1,13 @@
 #ifndef OSCFG_H
 #define OSCFG_H
 
-#define MLQ_SCHED 1
+#define MLQ_SCHED 140
 #define MAX_PRIO 140
 
 #define MM_PAGING
-//#define MM_FIXED_MEMSZ
-//#define VMDBG 1
-//#define MMDBG 1
+// #define MM_FIXED_MEMSZ
+#define VMDBG 1
+#define MMDBG 1
 #define IODUMP 1
 #define PAGETBL_DUMP 1
 
